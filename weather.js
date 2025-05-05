c268dbca67bca04c9a4f3c1ff76e2b81acdb9578
buori <buoriduck@naver.com>
2025-05-05 19:41:22 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,22 +1,19 @@
-// ↓ 자신의 디코딩된 서비스 키를 아래에 입력하세요.
 const API_KEY = "UX0oUh7ZWRuQW2qnng2rzw8U14zs2TmY9DYi8Eqs0SDQpo1wMe0l/BtVqaYcl7LYEJP0wysrk3xmQFgeE5iTJQ==";
 const forecastContainer = document.getElementById("forecast");
 
-// 예보 조회 날짜와 시간
-const baseDate = "20250505"; // YYYYMMDD
-const times = ["0100", "0600", "1100", "1600", "2100"];
+const baseDate = "20250505";
+const times = ["0100", "0400", "0700", "1000", "1300", "1600", "1900", "2200"];
 
 async function fetchWeather() {
   let html = "";
 
   for (let time of times) {
-    const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${baseDate}&base_time=0500&nx=60&ny=127`;
+    const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${API_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${baseDate}&base_time=0500&nx=61&ny=120`;
 
     const res = await fetch(url);
     const data = await res.json();
     const items = data.response.body.items.item;
 
-    // 특정 시간 데이터만 필터링
     const filtered = items.filter(i => i.fcstTime === time);
 
     const temp = filtered.find(i => i.category === "TMP")?.fcstValue ?? "N/A";
